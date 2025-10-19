bcb0d29f9a33ec989b5368c51759ed4511b21d16
BRAGADEESH V <bragadeesh2005@gmail.com>
2025-10-19 22:09:52 +0530

    chnages daa

--- server.js
+++ server.js
@@ -14,31 +14,16 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-const allowedOrigins =
-  process.env.NODE_ENV === "production"
-    ? [
-        "https://abacus-space.vercel.app",
-        "https://abacuspaces.com",
-        "https://abacus-backend-chi.vercel.app"
-      ]
-    : ["http://localhost:3000", "http://localhost:3001"];
-
+// Middleware
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like Postman or curl)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed for this origin"));
-      }
-    },
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://abacus-space.vercel.app"
+        : ["http://localhost:3000", "http://localhost:3001"],
     credentials: true,
   })
 );
-
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
